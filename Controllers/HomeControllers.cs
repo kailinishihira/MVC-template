a37d7d5b96ea5a3799344dab80ead2accf0034c0
Kaili <kailinishihira@gmail.com>
2017-08-02 18:01:30 -0700

    Add 3

--- Controllers/HomeControllers.cs
+++ Controllers/HomeControllers.cs
@@ -1,9 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
 
-namespace FileName.Controllers
+namespace ToDoList.Controllers
 {
     public class HomeController : Controller
     {
-        public string Hello() { return "Hello friend!"; }
+
+        [Route("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/task/list")]
+        public ActionResult TaskList()
+        {
+          Task newTask = new Task(Request.Query["new-task"]);
+          return View(newTask);
+        }
+
+        [HttpPost("/task/create")]
+        public ActionResult CreateTask()
+        {
+          Task newTask = new Task (Request.Form["new-task"]);
+          newTask.Save();
+          return View(newTask);
+        }
+
     }
 }
